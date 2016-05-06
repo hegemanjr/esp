c3e8c509582b41695bedb15512f83023d7906391
Jeff Hegeman <jhegeman2@gmail.com>
2016-05-05 18:09:13 -0700

    Override footer for coptright

---
+++ wp-content/themes/popper_child/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package popper
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			&copy; <?php echo date('Y'); ?>
+			<span class="sep"> | </span>
+			<a href="index.php">Eagle Springs Pub</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
